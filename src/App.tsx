d6d23929692c57249d799911c38de495ef737334
Haixiang6123 <haixiang6123@gmail.com>
2019-03-24 23:51:03 -0700

    Add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          App
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </div>
     );
   }
