e963aa1188bc461babc071293ff23210f13fe411
Haixiang6123 <haixiang6123@gmail.com>
2019-03-26 19:43:12 -0700

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import Home from './pages/Home/Home'
 import './App.scss'
+import {BrowserRouter as Router, Route} from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home/>
-      </div>
+      <Router>
+        <Route path="/" exact={true} component={Home}/>
+      </Router>
     );
   }
 }
