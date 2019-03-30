c1cc93f88df339dbe438479751e23f537cd3eb2c
Haixiang6123 <haixiang6123@gmail.com>
2019-03-29 21:24:44 -0700

    Finish Login page layout

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,43 @@
 import * as React from 'react'
+// Material UI
+import TextField from '@material-ui/core/TextField'
+// Types
+import {ILoginProps, ILoginState} from "./index"
+// Styles
+import {Wrapper, LoginButton, BackgroundImage} from './styles'
 
-class Login extends React.Component {
+class Login extends React.Component<ILoginProps, ILoginState> {
+  constructor(props: ILoginProps) {
+    super(props);
+    this.state = {
+      subId: ''
+    }
+  }
+
+  private onChangeSubId = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      subId: event.target.value
+    })
+  }
+
   public render() {
+    const {subId} = this.state
     return (
-      <div>Login</div>
+      <Wrapper>
+        <BackgroundImage src="https://i.loli.net/2019/03/30/5c9eeddca1a43.jpg" alt="bg"/>
+        <section>
+          <TextField
+            id="standard-name"
+            label="Login With User Name"
+            value={subId}
+            onChange={this.onChangeSubId}
+            margin="normal"
+          />
+        </section>
+        <LoginButton variant="contained" color="primary">
+          Login
+        </LoginButton>
+      </Wrapper>
     )
   }
 }
---
+++ src/pages/Login/index.d.ts
@@ -0,0 +1,6 @@
+export interface ILoginProps {
+
+}
+export interface ILoginState {
+  subId: string
+}
