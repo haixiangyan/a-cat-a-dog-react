ee2c1abfa3548c89b1e80e3d11e3f547ab2546b2
Haixiang6123 <haixiang6123@gmail.com>
2019-03-29 20:09:56 -0700

    Add analysis pie chart

--- src/components/Analysis/Analysis.tsx
+++ src/components/Analysis/Analysis.tsx
@@ -3,6 +3,8 @@ import * as React from 'react'
 import echarts from 'echarts'
 // Types
 import {IAnalysisProps, IAnalysisState} from "./index"
+// Helpers
+import {getOptions} from "./echartsHelpers"
 
 class Analysis extends React.Component<IAnalysisProps, IAnalysisState> {
   constructor(props: IAnalysisProps) {
@@ -11,51 +13,30 @@ class Analysis extends React.Component<IAnalysisProps, IAnalysisState> {
 
   private formatDate = (dateStr: string): string => {
     const date = new Date(dateStr)
-    return `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear()-2000}`
+    return `${date.getMonth() + 1}-${date.getDate()}-${date.getFullYear() - 2000}`
+  }
+
+  public componentDidMount(): void {
+    this.drawPie()
   }
 
   private drawPie = () => {
-    const pieEl: HTMLDivElement|null = document.querySelector('image-analysis-pie')
+    const pieEl: HTMLCanvasElement | null = document.querySelector('#image-analysis-pie')
     if (!pieEl) {
       return
     }
     const myChart = echarts.init(pieEl);
-
-    const option = {
-      title: {
-        text: 'ECharts 入门示例'
-      },
-      tooltip: {},
-      legend: {
-        data:['销量']
-      },
-      xAxis: {
-        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-      },
-      yAxis: {},
-      series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-      }]
-    };
-
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
+    const options: any = getOptions(this.props.imageAnalysis)
+    myChart.setOption(options);
   }
 
   public render() {
     const {imageAnalysis} = this.props
     return (
       <div>
-        {/*{*/}
-          {/*imageAnalysis.labels && imageAnalysis.labels.map(label =>*/}
-            {/*<p>Name: {label.Name}, Confidence: {label.Confidence}</p>*/}
-          {/*)*/}
-        {/*}*/}
-        <div id="image-analysis-pie"/>
-        <p><strong>Vendor: </strong>{imageAnalysis.vendor}</p>
-        <p><strong>Created At</strong>{this.formatDate(imageAnalysis.created_at)}</p>
+        <div id="image-analysis-pie" style={{height: 300}}/>
+        <p style={{marginBottom: 12}}><strong>Vendor: </strong>{imageAnalysis.vendor}</p>
+        <p><strong>Created At: </strong>{this.formatDate(imageAnalysis.created_at)}</p>
       </div>
     )
   }
---
+++ src/components/Analysis/echartsHelpers.ts
@@ -0,0 +1,41 @@
+import {IImageAnalysis} from "../../env"
+
+export const getOptions = (imageAnalysis: IImageAnalysis) => {
+  const xLabels = imageAnalysis.labels
+    .map(label => label.Name)
+  const yValues = imageAnalysis.labels
+    .map(label => Number(Number(label.Confidence).toFixed(2)))
+  const data = imageAnalysis.labels
+    .map(label => ({
+      value: Number(label.Confidence.toFixed(2)),
+      name: label.Name
+    }))
+
+  return {
+    title: {
+      text: 'Image Analysis',
+      subtext: `ID: ${imageAnalysis.image_id}`,
+      x: 'center'
+    },
+    tooltip: {
+      trigger: 'item',
+      formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    series: [
+      {
+        name: 'Confidence',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '60%'],
+        data: data,
+        itemStyle: {
+          emphasis: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+}
